8374af358c16723efd0118686ceca9ed8bc226ba
Smriti Goyal <85681121+Smritigoyal07@users.noreply.github.com>
2022-07-15 23:45:40 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -7,35 +7,94 @@ class Solution {
   public:
     // Function to detect cycle in an undirected graph.
     
-    bool check(int node, int parent , vector<int> &visited , vector<int> adj[]){
-        visited[node] = 1;
-        for(auto it: adj[node]){
-            if(visited[it] == 0 ){
-                if(check(it,node,visited,adj)){
-                    return true;
+    bool lol(int i,int V,vector<int>&vis,vector<int> adj[])
+    {
+        
+        queue<pair<int,int>>q;
+        // vis[i]=true;
+        q.push(make_pair(i,-1));
+        
+        while(!q.empty())
+        {
+            int curr=q.front().first;
+            int prev=q.front().second;
+            vis[i]=1;
+            q.pop();
+            
+            for(auto j:adj[curr])
+            {
+                if(!vis[j])
+                {
+                    vis[j]=1;
+                    q.push(make_pair(j,curr));
+                }
+                else if( vis[j]==1 && prev!=j)
+                {
+                    return true; 
                 }
             }
-            else if(it!=parent){
-                return true ;
+            
+        }
+        
+        return false;
+        
+    }    
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        // queue<pair<int,int>>q;
+        vector<int>vis(V,0);
+        for(int i=0 ; i<V ; i++)
+        {
+            if(vis[i]==0)
+            {
+                if(lol(i,V,vis,adj))
+                {
+                    return true;
+                }
             }
         }
         
         return false;
     }
     
-    bool isCycle(int V, vector<int> adj[]) {
-        vector<int> visited(V,0);
+    // bool check(int i, vector<int> &visited , vector<int> adj[]){
+    //     queue<pair<int,int>> q;
+    //     visited[i] = 1;
+    //     q.push({1,-1});
         
-        for(int i = 0 ; i < V ; i++){
-            if(!visited[i]){
-                if(check(i,-1,visited,adj)){
-                    return true;
-                }
-            }
-        }
+    //     while(!q.empty()){
+    //         int node = q.front().first;
+    //         int parent = q.front().second;
+            
+    //         for(auto it: adj[node]){
+    //             if(visited[it] == 0 ){
+    //                 visited[it] = 1;
+    //                 q.push({it,node});
+    //             }
+    //             else if(it!=parent){
+    //                 return true ;
+    //             }
+    //         }
+            
+    //         return false;
+    //     }
         
-        return false;
-    }
+    // }
+    
+    // bool isCycle(int V, vector<int> adj[]) {
+    //     vector<int> visited(V,0);
+        
+    //     for(int i = 0 ; i < V ; i++){
+    //         if(!visited[i]){
+    //             if(check(i,visited,adj)){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+        
+    //     return false;
+    // }
 };
 
 // { Driver Code Starts.
